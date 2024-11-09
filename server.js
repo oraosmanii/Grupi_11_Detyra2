63fe3e75fb7f15b41f476321da70cbd5700dfc28
oraosmanii <oraosmani@gmail.com>
2024-11-09 21:03:54 +0100

    log functions & client queues functions

--- server.js
+++ server.js
@@ -24,10 +24,40 @@ const server = net.createServer((socket) => {
     logConnection(socket);
 
    
-socket.setTimeout(IDLE_TIMEOUT);
-socket.on('timeout', () => {
-    console.log(`\nClient ${socket.remoteAddress}:${socket.remotePort} disconnected due to inactivity.`);
-    removeFromQueue(socket); 
-    socket.end(); 
+    socket.setTimeout(IDLE_TIMEOUT);
+    socket.on('timeout', () => {
+      console.log(`\nClient ${socket.remoteAddress}:${socket.remotePort} disconnected due to inactivity.`);
+      removeFromQueue(socket); 
+      socket.end(); 
+    });
+    
 
 });
+
+function processNextClient() {
+    
+    const nextClient = fullAccessQueue.length > 0 ? fullAccessQueue.shift() : readQueue.shift();
+    if (nextClient) {
+        nextClient.write('You are now being served.\n');
+    }
+}
+
+function removeFromQueue(socket) {
+    if (socket.clientType === 'FULL_ACCESS') {
+        fullAccessQueue = fullAccessQueue.filter(client => client !== socket);
+    } else if (socket.clientType === 'read') {
+        readQueue = readQueue.filter(client => client !== socket);
+    }
+}
+
+
+function logConnection(socket) {
+    const logData = `Connected: ${socket.remoteAddress} at ${new Date()}\n`;
+    fs.appendFileSync('connections.log', logData);
+}
+
+
+function logRequest(socket, data) {
+    const logData = `Request from ${socket.remoteAddress} at ${new Date()}: ${data}\n`;
+    fs.appendFileSync('requests.log', logData);
+}
